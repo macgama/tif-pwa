9f3e6b67877a6c29ca98a0259ffbd3ee2732ee75
jeanquark <jm.kleger@gmail.com>
2018-12-02 13:51:40 +0100

    Added .setup-firebaserc file

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -7,25 +7,25 @@ if (config.dev) {
     project_id: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
     private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
+    client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
+    client_id: "107417583891627821090",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2w96f%40tif-pwa.iam.gserviceaccount.com"
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
   };
 } else {
   module.exports = {
     type: "service_account",
     project_id: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: JSON.parse(process.env.PRIVATE_KEY),
-    // private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
+    // private_key: JSON.parse(process.env.PRIVATE_KEY),
+    private_key: process.env.PRIVATE_KEY,
+    client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
+    client_id: "107417583891627821090",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2w96f%40tif-pwa.iam.gserviceaccount.com"
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
   };
 }
