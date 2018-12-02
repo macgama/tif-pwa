4bc426ea4bad3c7f3bd22223ae03b1b8c3b5c7b3
jeanquark <jm.kleger@gmail.com>
2018-12-02 22:52:05 +0100

    Changed firebase-client-init.js

--- plugins/firebase-client-init.js
+++ plugins/firebase-client-init.js
@@ -1,23 +1,23 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
-// var config = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   databaseURL: process.env.DATABASE_URL,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID
-// };
-
 var config = {
-    apiKey: "AIzaSyAM6LAjoitrT8oBtMvK9JR7dIUDlcMJR7g",
-    authDomain: "tif-pwa-a7970.firebaseapp.com",
-    databaseURL: "https://tif-pwa-a7970.firebaseio.com",
-    projectId: "tif-pwa-a7970",
-    storageBucket: "tif-pwa-a7970.appspot.com",
-    messagingSenderId: "861826276315"
-}
+  	apiKey: "AIzaSyAM6LAjoitrT8oBtMvK9JR7dIUDlcMJR7g",
+  	authDomain: process.env.AUTH_DOMAIN,
+  	databaseURL: process.env.DATABASE_URL,
+  	projectId: process.env.PROJECT_ID,
+  	storageBucket: process.env.STORAGE_BUCKET,
+  	messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+// var config = {
+//     apiKey: "AIzaSyAM6LAjoitrT8oBtMvK9JR7dIUDlcMJR7g",
+//     authDomain: "tif-pwa-a7970.firebaseapp.com",
+//     databaseURL: "https://tif-pwa-a7970.firebaseio.com",
+//     projectId: "tif-pwa-a7970",
+//     storageBucket: "tif-pwa-a7970.appspot.com",
+//     messagingSenderId: "861826276315"
+// }
 
 export default (!firebase.apps.length
   ? firebase.initializeApp(config)
--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -3,31 +3,31 @@ let config = require("./nuxt.config.js");
 
 if (config.dev) {
     console.log('config.dev')
-  module.exports = {
-    type: "service_account",
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
-    client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
-    client_id: "107417583891627821090",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
-  };
+    module.exports = {
+        type: "service_account",
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
+        client_id: "107417583891627821090",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
+    };
 } else {
     console.log('!config.dev')
-  module.exports = {
-    type: "service_account",
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: JSON.parse(process.env.PRIVATE_KEY),
-    // private_key: process.env.PRIVATE_KEY,
-    client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
-    client_id: "107417583891627821090",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
-  };
+    module.exports = {
+        type: "service_account",
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: JSON.parse(process.env.PRIVATE_KEY),
+        // private_key: process.env.PRIVATE_KEY,
+        client_email: "firebase-adminsdk-b6n32@tif-pwa-a7970.iam.gserviceaccount.com",
+        client_id: "107417583891627821090",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-b6n32%40tif-pwa-a7970.iam.gserviceaccount.com"
+    };
 }
