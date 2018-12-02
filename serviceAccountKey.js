3c8987073e05fddb9090142665f59fc3ee9185b6
jeanquark <jm.kleger@gmail.com>
2018-12-02 01:27:07 +0100

    Edited serviceAccountKey file for JSON.parse()

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -14,14 +14,14 @@ if (config.dev) {
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url:
       "https//www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-li5wg%40fir-auth-vue-1aa66.iam.gserviceaccount.com",
-    football_api_key: process.env.FOOTBALL_API_KEY
+    // football_api_key: process.env.FOOTBALL_API_KEY
   };
 } else {
   module.exports = {
     type: "service_account",
     project_id: process.env.PROJECT_ID,
-    // private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: JSON.parse(process.env.PRIVATE_KEY),
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    // private_key: JSON.parse(process.env.PRIVATE_KEY),
     private_key: process.env.PRIVATE_KEY,
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
@@ -30,7 +30,7 @@ if (config.dev) {
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url:
       "https//www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-li5wg%40fir-auth-vue-1aa66.iam.gserviceaccount.com",
-    football_api_key: process.env.FOOTBALL_API_KEY
+    // football_api_key: process.env.FOOTBALL_API_KEY
     // apiKey: process.env.API_KEY,
     // authDomain: process.env.AUTH_DOMAIN,
     // databaseURL: process.env.DATABASE_URL,
